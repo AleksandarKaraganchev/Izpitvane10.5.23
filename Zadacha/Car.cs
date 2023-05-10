f45bd565ce74fea6f335455c2c94e4fe351de98b
STUDENTS-1212-12 <fifkobg@gmail.com>
2023-05-10 11:31:42 +0300

    Add project files.

---
+++ Zadacha/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Security.Policy;
+
+namespace Zadacha
+{
+    public class Car
+    {
+		private string brand;
+		private static int count;
+
+		public static int Count
+		{
+			get { return count; }
+		}
+
+		public string Brand
+		{
+			get { return brand; }
+			set { brand = value; }
+		}
+		private string model;
+		public string Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+		private string registrationNumber;
+		public string RegistrationNumber
+		{
+			get { return registrationNumber; }
+			set { registrationNumber = value; }
+		}
+		private int yearofProduction;
+		public int YearofProduction
+        {
+			get { return yearofProduction; }
+			set { yearofProduction = value; }
+		}
+		public void Move()
+		{
+			Console.WriteLine($"[{this.Brand}] , [{this.Model}] Brym – brum -brum!!!");
+		}
+		public void IfEcoFriendly()
+		{
+			if (this.YearofProduction >= 2000)
+			{
+				Console.WriteLine("i'm a eco friendly car !!!:) [ECO]");
+			}
+			else
+			{
+				Console.WriteLine("I'm not a eco car :(");
+			}
+		}
+		public void Introduction()
+		{
+			Console.WriteLine($"Brand: [{this.Brand}], Model: [{this.Model}], Registration number: [{this.RegistrationNumber}], Year of production: [{this.YearofProduction}]");
+		}
+
+		public Car(string brand, string model, string registrationnum, int yearofproduction)
+		{
+			this.brand = brand;
+			this.model = model;
+			this.registrationNumber = registrationnum;
+			this.yearofProduction = yearofproduction;
+			count++;
+		}
+	}
+}
---
+++ Zadacha/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //logic of the task number 1
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            //Car car1 = new Car();
+            //Console.Write("Enter brand of car 1: ");
+            //car1.Brand = Console.ReadLine();
+            //Console.Write("Enter model of car 1: ");
+            //car1.Model = Console.ReadLine();
+            //Console.Write("Enter registration number of car1: ");
+            //car1.RegistrationNumber = Console.ReadLine();
+            //Console.Write("Enter year of production of your car {after 2000 is eco below is not eco!}: ");
+            //car1.YearofProduction = int.Parse(Console.ReadLine());
+            //Car car2 = new Car();
+            //Console.Write("Enter brand of car 1: ");
+            //car2.Brand = Console.ReadLine();
+            //Console.Write("Enter model of car 1: ");
+            //car2.Model = Console.ReadLine();
+            //Console.Write("Enter registration number of car1: ");
+            //car2.RegistrationNumber = Console.ReadLine();
+            //Console.Write("Enter year of production of your car {after 2000 is eco below is not eco!}: ");
+            //car2.YearofProduction = int.Parse(Console.ReadLine());
+            //Console.WriteLine("---------------------------------------------------------------");
+            //car1.Move();
+            //car1.IfEcoFriendly();
+            //car1.Introduction();
+            //Console.WriteLine();
+            //car2.Move();
+            //car2.IfEcoFriendly();
+            //car2.Introduction();
+            //Console.WriteLine("---------------------------------------------------------------");
+            //int numberOfParticipants = 2;
+            //Console.WriteLine($"The number of participants is {numberOfParticipants}");
+
+            //logic of the task number 2
+
+            // Console.Write("Enter number of cars in the race: ");
+            //int n = int.Parse(Console.ReadLine());
+            //counter = 0;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Car car = new Car();
+            //    Console.Write("Enter brand of car: ");
+            //    car.Brand = Console.ReadLine();
+            //    Console.Write("Enter model of car: ");
+            //    car.Model = Console.ReadLine();
+            //    Console.Write("Enter registration number of the car: ");
+            //    car.RegistrationNumber = Console.ReadLine();
+            //    Console.Write("Enter year of production of  car {after 2000 is eco, below is not eco!}: ");
+            //    car.YearofProduction = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("---------------------------------------------------------------");
+            //    car.Move();
+            //    car.IfEcoFriendly();
+            //    car.Introduction();
+            //    Console.WriteLine("---------------------------------------------------------------");
+            //    counter++;
+            //}
+            //Console.WriteLine($"Number of participants in the race!!!: {counter}");
+
+            //Console.WriteLine($"The number of participants in the race is: {Car.Count}");
+
+            //logic 3 - the best one yet
+
+            List<Car> list = new List<Car>();
+            Console.Write("Enter number of cars: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter brand: ");
+                string brand = Console.ReadLine();
+                Console.Write("Enter model: ");
+                string model = Console.ReadLine();
+                Console.Write("Enter registration number of car: ");
+                string registrationnumber = Console.ReadLine();
+                Console.Write("Enter year of production of your car {after 2000 is eco below is not eco!}: ");
+                int year = int.Parse(Console.ReadLine());
+                Car car = new Car(brand, model, registrationnumber, year);
+                list.Add(car);
+            }
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("-----------------------------------------------------------------------");
+            foreach (var example in list)
+            {
+                example.Move();
+                example.IfEcoFriendly();
+                example.Introduction();
+                Console.WriteLine("-----------------------------------------------------------------------");
+            }
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"The number of participants in the race is: {Car.Count}!!!");
+            Console.WriteLine();
+        }
+    }
+}
